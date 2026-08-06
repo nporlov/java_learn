91eca68d7314ab2f955b87b64d71b64827387ecd
Nikita <nporlov@gmail.com>
2026-08-06 15:29:01 +0400

    added_ls_command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,17 +4,35 @@ import java.util.Scanner;
 
 public class Main {
     static void main(String[] args) {
-        Path myFolder = Paths.get("C:\\Dev\\java\\workspace");
+        // INPUT
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
         try {
-            DirectoryStream <Path> stream = Files.newDirectoryStream(myFolder);
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
-            System.out.println(input);
-            for (Path path : stream) {
-                System.out.println(path.getFileName());
+            // SPLIT COMMAND AND DIRECTORY
+            String[] inputs = input.split(" ");
+            String command = inputs[0];
+            Path folder = Paths.get(inputs[1]);
+
+            // EXECUTE COMMANDS
+            try {
+                switch (command) {
+                    case "ls":
+                        listFiles(folder);
+                }
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
             }
-        } catch (IOException e) {
+        }
+        catch (Exception e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    static void listFiles(Path folder) throws IOException {
+        DirectoryStream <Path> stream = Files.newDirectoryStream(folder);
+        for (Path path : stream) {
+            System.out.println(path.getFileName());
         }
     }
 }
