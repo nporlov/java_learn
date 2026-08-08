8f8a0979c00de962eeec38a8e2644247a05bab33
Nikita <nporlov@gmail.com>
2026-08-08 13:14:50 +0400

    handled_exceptions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,38 +1,32 @@
 import java.io.IOException;
 import java.nio.file.*;
-import java.util.Scanner;
 
 public class Main {
     static void main(String[] args) {
-        // INPUT
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        String command = null;
+        Path folder = null;
+        try {
+            command = args[0];
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("No command entered. " + e);
+        }
 
         try {
-            // SPLIT COMMAND AND DIRECTORY
-            String[] inputs = input.split(" ");
-            String command = inputs[0];
-            Path folder = Paths.get(inputs[1]);
+            folder = Paths.get(args[1]);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("No argument entered. " + e);
+        } catch (InvalidPathException e) {
+            System.out.println("Invalid path entered. " + e);
+        }
 
-            // EXECUTE COMMANDS
-            try {
-                switch (command) {
-                    case "ls":
-                        listFiles(folder);
-                }
-            } catch (IOException e) {
-                System.err.println(e.getMessage());
+        try {
+            switch (command) {
+                case "ls": FilesAdapter.getFileList(folder);
             }
-        }
-        catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    static void listFiles(Path folder) throws IOException {
-        DirectoryStream <Path> stream = Files.newDirectoryStream(folder);
-        for (Path path : stream) {
-            System.out.println(path.getFileName());
+        } catch (IOException e) {
+            System.out.println("Error opening files list. " + e);
+        } catch (NullPointerException e) {
+            System.out.println("No files list found. " + e);
         }
     }
 }
